2734736c6a7b9d1a8393698071af6717d493d36d
Ss <sen999999888@163.com>
2024-05-25 00:20:08 +0800

    update port

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() {
         .route("/claim", post(claim));
 
     // init listener
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
